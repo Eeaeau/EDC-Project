9e96239ba6aa596e905ea04b3d6fef057e127be3
Sebastian <sebskogen@gmail.com>
2021-04-30 23:42:17 +0200

    Cleaned up and commented iris code

--- Iris_TTT4275/iris_classification.py
+++ Iris_TTT4275/iris_classification.py
@@ -20,10 +20,6 @@ with open('Iris_TTT4275/class_2.txt', 'r') as f:
 with open('Iris_TTT4275/class_3.txt', 'r') as f:
     x3all = np.array([[float(num) for num in line.split(',')] for line in f])
 
-# dataset_pd = pd.read_fwf('Iris_TTT4275\iris.data', widths=[50, 50, 50])
-# print(dataset_pd)
-
-
 # ----------- constants
 num_classes = 3
 num_data = 50
@@ -66,76 +62,84 @@ def combined_mse(x, W, true_class):
     for k in range(N):
         for c in range(num_classes):
 
+            #  ----------- in between calculations of mse gradient --------- #
+            # set the correct true class
             t = true_class[c]
 
+            # get current linear classfier
             g_temp = linear_descriminat_classifier(x[c, k], W)
 
+            # increase "contrast" in classifications
             g_k = sigmoid(g_temp)
-            # print(t)
 
             gradgk_mse = g_k - t
-            # print("gradgk_mse:", gradgk_mse)
 
             gradzk_g = g_k*(1-g_k)  # elementwise
-            # print("gradzk_g:", gradzk_g)
 
             gradw_zk = x[c, k].reshape(1, num_attributes + 1)  # transposes
-            # print("gradw_zk: ", gradw_zk)
 
             temp = (gradgk_mse*gradzk_g).reshape(
                 num_classes, 1)  # make two dimentional, such that it can represent a matrix
 
-            # print("temp: ", temp)
+            # add final resulting matrix to gradient
             gradw_mse += temp @ gradw_zk  # matrix multi
 
+            # Calculate and add final contribution to MSE
             mse += np.matmul(gradgk_mse.T, gradgk_mse)
-
-    # print("gradw_mse", gradw_mse)
 
     return 1/2*mse, gradw_mse
 
 
-def train_LC(training_dataset, W_track, alpha=alpha, grad_mse_threshold=0.001, max_iterations=500, use_dynamic_alpha=True, plot_result=False):
+def train_LC(training_dataset, W_track, alpha=alpha, grad_mse_threshold=0.001,
+             max_iterations=500, use_dynamic_alpha=True, plot_result=False):
 
     mse_track = []
 
     true_classes = np.identity(num_classes, dtype=float)
 
-    i = 0
-
     gradw_mse = np.full(
         (num_classes, num_attributes + 1), grad_mse_threshold+1)
 
-    while((i < max_iterations) & ~((np.absolute(gradw_mse) < grad_mse_threshold).all())):
+    # Check if every element of gradient_w of MSE is less than
+    # the required threshold
+    threshold_condition = ~(
+        (np.absolute(gradw_mse) < grad_mse_threshold).all())
+
+    i = 0
+    # Itterate av long as max iteration and threshold condisions are not meet
+    while((i < max_iterations) & threshold_condition):
         mse, gradw_mse = combined_mse(
             training_dataset, W_track[-1], true_classes)
 
         W_track = np.append(
-            W_track, [W_track[-1] - (dynamic_alpha(alpha, i, max_iterations) if use_dynamic_alpha else alpha) * gradw_mse], axis=0)
+            W_track, [W_track[-1] -
+                      (dynamic_alpha(alpha, i, max_iterations)
+                       if use_dynamic_alpha else alpha) * gradw_mse], axis=0)
 
         mse_track.append(mse)
 
         i += 1
 
-    print("W: ", W_track[-1])
     if plot_result:
         plt.plot(mse_track)
         plt.xlabel("Iterations")
         plt.ylabel("MSE")
-        # plt.show()
+        plt.show()
 
     print("Completed training with ", i, " iterations")
 
     return W_track[-1]
 
 
-def get_confusion_matrix(W, training_dataset, alpha=alpha, plot_result=False):
+def get_confusion_matrix(W, testing_dataset, alpha=alpha, plot_result=False):
     confusion_matrix = np.zeros((num_classes, num_classes))
 
+    # evaluate confusion matrix based upon testingdataset
+    # with trained weights W for linear classifier
     for c in range(num_classes):
         for i in range(testing_size):
             prediction = np.argmax(
-                linear_descriminat_classifier(training_dataset[c][i], W))
+                linear_descriminat_classifier(testing_dataset[c][i], W))
             confusion_matrix[c, prediction] += 1
 
     if plot_result:
@@ -144,6 +148,7 @@ def get_confusion_matrix(W, training_dataset, alpha=alpha, plot_result=False):
 
         sns.heatmap(confusion_matrix,  annot=True, square=True)
 
+        # set correct ticks
         ax.xaxis.tick_bottom()
         plt.xticks(np.arange(3) + .5, rotation=45, labels=classes)
         ax.yaxis.tick_left()
@@ -161,15 +166,19 @@ def get_confusion_matrix(W, training_dataset, alpha=alpha, plot_result=False):
 
 
 def dynamic_alpha(initial_alpha, iteration, total_iterations):
-    return initial_alpha*np.exp(-iteration/(1*total_iterations))
+    # Calculate reduced alpha based upon current iteration and
+    # normalized by total number of iterations
+    return initial_alpha*np.exp(-iteration/(total_iterations))
 
 
 def calculate_error_rate(confusion_matrix):
-    # TODO: add check for non square matrix
-    dim = np.size(confusion_matrix, 0)
+
+    dim = np.size(confusion_matrix, axis=0)
     num_classifications_class = np.zeros((dim), dtype=float)
     error_rate = np.zeros(dim)
 
+    # Sum and normelize collective miss-classifications
+    # for each class
     for row in range(dim):
         for col in range(dim):
             num_classifications_class[row] += confusion_matrix[row, col]
@@ -179,8 +188,6 @@ def calculate_error_rate(confusion_matrix):
 
     return error_rate
 
-    # for i in range(dimention):
-
 
 def plot_hist(dataset):
 
@@ -188,6 +195,7 @@ def plot_hist(dataset):
     fig, axs = plt.subplots(2, 2)
 
     row = 0
+    # plot histograms for each attribute in a square 2x2 grid
     for a in range(num_attributes):
         for c in range(num_classes):
             axs[a % 2, row].hist(dataset[c, :, a], bins=12, alpha=0.7)
@@ -197,18 +205,10 @@ def plot_hist(dataset):
         axs[a % 2, row].legend(classes, loc='upper right')
         if a == 1:
             row += 1
+
     fig.set_size_inches(3*3, 2*3)
     fig.tight_layout(pad=.2)
-    # for c in range(num_classes):
-    #     for a in range(num_attributes):
-    #         axs[c, a].hist(dataset[c, :, a], bins=10)
-    #         axs[c, a].set_title(classes[c]+" "+attributes[a])
-    # fig.tight_layout(pad=.2)
 
-    # fig.show()
-    # print(dataset[0, :, 0])
-    # plt.hist(dataset[0, :, 0],)
-    # sns.displot(dataset[0, :, 0])
     plt.show()
 
 
@@ -216,9 +216,10 @@ def reduce_dataset_attributes(dataset, removed_attribute_indexs):
 
     reduced_dataset = np.delete(dataset, removed_attribute_indexs, axis=2)
 
+    # get current number of attributes compensated for extra dimmetion added
     current_num_attributes = np.size(dataset, axis=2) - 1
 
-    print("current_num_attributes: ", current_num_attributes)
+    print("Number of attributes is now: ", current_num_attributes)
 
     num_attributes = current_num_attributes - len(removed_attribute_indexs)
 
@@ -228,16 +229,11 @@ def reduce_dataset_attributes(dataset, removed_attribute_indexs):
 def format_dataset(datasets):
     formated_dataset = np.empty((num_classes, num_data, num_attributes+1))
 
-    joined_dataset = np.stack(datasets, axis=0)
+    joined_dataset = np.stack(datasets, axis=0)  # merge dataset for each class
 
     for c in range(len(datasets)):
         formated_dataset[c] = np.array([np.append(data, 1)
                                         for data in joined_dataset[c]])
-
-    # dataset[0] = np.array([np.append(data, 1) for data in x1all])
-
-    # dataset[1] = np.array([np.append(data, 1) for data in x2all])
-    # dataset[2] = np.array([np.append(data, 1) for data in x3all])
 
     return formated_dataset
 
@@ -248,29 +244,16 @@ def format_dataset(datasets):
 
 dataset = format_dataset([x1all, x2all, x3all])
 
+# plot histograms
+plot_hist(dataset)
 
-# W_track = np.full([1, num_classes, num_attributes+1], 0)
-
-# alphas = [0.5, 0.1, 0.05, 0.01]
-
-# fig = plt.figure(figsize=(16/2, 9/2))
-# sns.set_theme(style="darkgrid")
-# for alpha in alphas:
-
-#     W_track_final = train_LC(training_dataset, W_track,
-#  alpha=alpha, total_iterations=1000, plot_result=True)
-
-
+# set of different alphas to iteratibly conduct testing on
 alphas = [0.5, 0.1, 0.05, 0.01]
 
+# set of different feature indeces to iteratibly remove
+features_to_remove = [[], [2], [0, 2], [0, 1, 2]]
 
-# plot histograms
-# plot_hist(dataset)
-
-# features_to_remove = [[], [2], [0, 2], [0, 1, 2]]
-# features_to_remove = [[], [1], [1, 2], [0, 1, 2]]
-features_to_remove = [[], [0], [0, 1], [0, 1, 2]]
-
+# ----------- Evaluation for different sets of features removed ----------- #
 for removed in features_to_remove:
 
     # set alternative dataset with reduced number of attributes
@@ -286,14 +269,12 @@ for removed in features_to_remove:
     W_track_final = train_LC(training_dataset, W_track,
                              alpha=alpha, grad_mse_threshold=0.07, max_iterations=10000, use_dynamic_alpha=True,  plot_result=False)
 
-# plt.legend(["alpha = "+str(alpha) for alpha in alphas], loc="upper right")
-# plt.show()
-
     confusion_matrix = get_confusion_matrix(
         W_track_final, testing_dataset, alpha, True)
 
-    print("Confusion matrix", confusion_matrix)
+    print("Confusion matrix: ", confusion_matrix)
 
     error_rate = calculate_error_rate(confusion_matrix)
-    print("Error rate", error_rate)
-    print("final W", W_track_final)
+
+    print("Error rate: ", error_rate)
+    print("Final W: ", W_track_final)
